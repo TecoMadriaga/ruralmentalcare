9829b6d89171a239bc0ac9baaa00f6c498086593
Sergio <smadriagau@outlook.com>
2023-11-16 09:08:32 -0300

    Add new templates and views for listing professionals, scheduling appointments, and displaying educational resources.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Modelo para usuarios (pacientes)
+class Paciente(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    fecha_nacimiento = models.DateField()
+    telefono = models.CharField(max_length=15)
+    direccion = models.TextField()
+    historial_medico = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.usuario.username
+
+# Modelo para profesionales
+class Profesional(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    especialidad = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=15)
+    biografia = models.TextField()
+    calificacion_promedio = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.usuario.username
+
+# Modelo para citas
+class Cita(models.Model):
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+    profesional = models.ForeignKey(Profesional, on_delete=models.CASCADE)
+    fecha_hora_inicio = models.DateTimeField()
+    fecha_hora_fin = models.DateTimeField()
+    estado = models.CharField(max_length=50)
+    resumen = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"Cita entre {self.paciente.usuario.username} y {self.profesional.usuario.username}"
+
+# Modelo para recursos educativos
+class RecursoEducativo(models.Model):
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    tipo = models.CharField(max_length=50)
+    enlace = models.URLField()
+    fecha_publicacion = models.DateField()
+
+    def __str__(self):
+        return self.titulo
+
+# Modelo para feedback/reseñas
+class Feedback(models.Model):
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+    profesional = models.ForeignKey(Profesional, on_delete=models.CASCADE)
+    comentario = models.TextField()
+    calificacion = models.IntegerField()
+    fecha = models.DateField()
+
+    def __str__(self):
+        return f"Feedback de {self.paciente.usuario.username} sobre {self.profesional.usuario.username}"
--- app/views.py
+++ app/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Profesional, Cita, RecursoEducativo
+from django.contrib.auth.decorators import login_required
+from .forms import CitaForm
 
-# Create your views here.
+# Vista para listar profesionales
+def lista_profesionales(request):
+    profesionales = Profesional.objects.all()
+    return render(request, 'profesionales.html', {'profesionales': profesionales})
+
+# Vista para ver detalles de un profesional
+def detalle_profesional(request, pk):
+    profesional = get_object_or_404(Profesional, pk=pk)
+    return render(request, 'detalle_profesional.html', {'profesional': profesional})
+
+# Vista para agendar una nueva cita
+@login_required
+def nueva_cita(request, profesional_id):
+    profesional = get_object_or_404(Profesional, pk=profesional_id)
+    if request.method == 'POST':
+        form = CitaForm(request.POST)
+        if form.is_valid():
+            cita = form.save(commit=False)
+            cita.paciente = request.user.paciente
+            cita.profesional = profesional
+            cita.save()
+            return redirect('detalle_cita', pk=cita.pk)
+    else:
+        form = CitaForm()
+    return render(request, 'nueva_cita.html', {'form': form, 'profesional': profesional})
+
+# Vista para listar recursos educativos
+def lista_recursos(request):
+    recursos = RecursoEducativo.objects.all()
+    return render(request, 'recursos.html', {'recursos': recursos})
